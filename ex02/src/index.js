b385e7ddaa578083e0f730f0dbafcf099665f84b
zmarceta <zmarceta83@gmail.com>
2021-03-11 09:24:54 +0100

    test

---
+++ ex02/src/index.js
@@ -0,0 +1,17 @@
+function main() {
+	// Only change code below this line
+	
+	// Lorem Ipsum 
+
+    /*
+    Lorem 
+    Ipsum
+    */
+	
+	// Only change code above this line
+	
+}
+
+console.log(main());
+
+module.exports = main;
